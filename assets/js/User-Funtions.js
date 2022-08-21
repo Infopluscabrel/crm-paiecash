a40b1fba774e734cdb333ba60f43c120eab654c2
bouagangdaleucelarinelafortune <80589055+celarinedaleu@users.noreply.github.com>
2022-08-21 15:34:38 +0100

    add article and revendeur ok

--- assets/js/User-Funtions.js
+++ assets/js/User-Funtions.js
@@ -1,102 +1,5 @@
 const path = "http://localhost:5000";
 
-//fonction pour enregistrer un article
-$('#saveArticle').livequery('submit',   function(e){ e.preventDefault() ; 
-    var nom = document.getElementById("nom").value;
-    var quantite = document.getElementById("quantite").value;
-    var prix= document.getElementById("prix").value;
-    var date = document.getElementById("date").value;
-    var formData = new FormData();
-    formData.append('nom', nom);
-    formData.append('prix', prix);
-    formData.append('quantite', quantite);
-    formData.append('CREATED_AT', date);
-    formData.append('proprietaire', "1");
-    
-  
- var data  = {nom: nom, prix: prix, quantite: quantite, CREATED_AT: date, proprietaire:"1"};
-  console.log(data);
-
-  $.post("http://localhost:5000/article/new", data, function(puerto){
-
-   console.log(puerto) ;
-  }, 'json');
-
-  return false;
-
-});
-
-//fonction pour lister les article
-// api url
-const api_url =
-	"http://localhost:5000/article/all";
-
-// Defining async function
-async function getapi(url) {
-	
-	// Storing response
-	const response = await fetch(url);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
-	}
-	show(data);
-}
-// Calling that async function
-getapi(api_url);
-
-// Function to hide the loader
-function hideloader() {
-	
-}
-// Function to define innerHTML for HTML table
-function show(data) {
-    console.log(data);
-
-    
-	let tab =
-		`<tr>
-        <th>#</th>
-		<th>Nom</th>
-        <th>Quantite</th>
-        <th>Date</th>
-        <th>Options</th>
-		</tr>`;
-	
-	// Loop to access all rows
-	 for (let r of data.data) {
-		tab += `<tr>
-    <td>${r.ID_PRODUIT} </td>      
-	<td>${r.NOM_PRODUIT} </td>
-	<td>${r.QUANTITE}</td>
-	<td>${r.CREATED_AT} </td>
-       
-    <td><ul class="list-inline m-0">
-                      
-    <li class="list-inline-item">
-      <button class="btn btn-success btn-sm " type="button" data-toggle="modal" data-target="#ajoutRevendeur" data-placement="top" title="Edit"
-        style="margin-bottom: 10px; vertical-align: baseline;"><i class="bi bi-pencil-square"></i>Editer</button>
-    </li>
-    <li class="list-inline-item">
-      <button class="btn btn-danger btn-sm " type="button" data-toggle="tooltip" data-placement="top"
-        title="Delete"><i class="bi bi-trash"></i>Supprimer</button>
-    </li>
-    <li class="list-inline-item" data-toggle="modal" data-target="#plusinfo"  >
-      <button type="button" class="btn btn-warning btn-sm">Voire plus</button>
-    </li>
-  </ul></td>  
-    </tr>`;
-	}
-  
-	// Setting innerHTML as tab variable
-	document.getElementById("list-article").innerHTML = tab;
-}
-
-
-
 // fonction pour enregister un revendeur
 $('#saveRevendeur').livequery('submit',   function(e){ e.preventDefault() ; 
     var client = document.getElementById("client").value;
@@ -186,14 +89,10 @@ adresse: adresse, precompte: precompte, ristourne: ristourne};
 
 });
 
-
-
- 
 //fonction pour recupérer la liste des revendeurs.
 // api url
-const api_url =
-	"http://localhost:5000/user/all";
 
+const api_url = "http://localhost:5000/user/all";
 // Defining async function
 async function getapi(url) {
 	
@@ -252,7 +151,7 @@ function show(data) {
     <td>${r.ID_USER} </td> 
     <td>SABC</td>   
 	<td>${r.NOM_USER} </td>
-	<td>${r.EMAIL}</td>
+	<td>${r.EMAIL}</td>s
 	<td>${r.LOGIN} </td>
     <td>${r.offre} </td>
     <td>${r.precompte} </td>
